3a1e2e336f3dafef48d1264e96430b860b13c919
Tim de Kievit <timdekievit@MBPvanTdeKievit.localdomain>
2024-03-11 12:57:17 +0100

    updated .gitignore

--- DatabaseSync/obj/Debug/net8.0/DatabaseSync.AssemblyInfo.cs
+++ DatabaseSync/obj/Debug/net8.0/DatabaseSync.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DatabaseSync")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e04cf01e4eebc0bcff09d126d1515543c14150eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0a22ff875558d7480355973d2e7cfddab06a035")]
 [assembly: System.Reflection.AssemblyProductAttribute("DatabaseSync")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DatabaseSync")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
